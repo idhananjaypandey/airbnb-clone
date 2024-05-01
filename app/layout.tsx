ce8cd3fb80ae41277529e19249558a30535ca4c1
Dhananjay Kumar Pandey <dhananjay.pandey72@gmail.com>
2024-05-01 23:36:18 +0530

    adding modal button components and hooks

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Nunito } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
 import ClientOnly from "./components/ClientOnly";
+import Modal from "./components/modals/Modal";
 
 // const inter = Inter({ subsets: ["latin"] });
 
@@ -25,6 +26,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
+          <Modal actionLabel="Submit" isOpen />
         <Navbar />
         </ClientOnly>
         {children}
