b0d4ed844da25b82d388b8e08c0250e8850eeaff
Dhananjay Kumar Pandey <dhananjay.pandey72@gmail.com>
2024-05-10 23:02:50 +0530

    github authentication with signup

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -2,14 +2,20 @@
 
 import Image from "next/image";
 
-const Avatar = () => {
+interface AvaratProps {
+  src: string | null | undefined
+}
+
+const Avatar: React.FC<AvaratProps> = ({
+  src
+}) => {
   return (
     <Image
       className=""
       height="30"
       width="30"
       alt="Avatar"
-      src="/images/placeholder.jpg"
+      src={src || "/images/placeholder.jpg"}
     />
   );
 };
